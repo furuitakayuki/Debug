4def7f7cc8d42a3a0e4060041cc4ce2bec10a493
furuitakayuki <maki1904@icloud.com>
2022-09-24 08:38:39 +0900

    デバッグ

--- app/src/main/java/jp/techacademy/takayuk/debug/MainActivity.kt
+++ app/src/main/java/jp/techacademy/takayuk/debug/MainActivity.kt
@@ -2,10 +2,24 @@ package jp.techacademy.takayuk.debug
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list = ArrayList<String>()
+        list.add("おはようございます")
+        list.add("こんにちは！")
+
+        // デバッグ用に追加する
+        for(i in list.indices) {
+            Log.d("ANDROID", "list[$i]=${list[i]}")
+        }
+
+        textView.text = list[1]
     }
 }
